7563f41086f0b8eb05de1ebca87bf7e2f8a027de
bharathqwer <144425505+bharathqwer@users.noreply.github.com>
2023-09-19 13:04:36 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,7 +1,34 @@
-n=int(input('enter the factorial number:'))
-fact=1
-for i in range(1,n+1):
-     fact=fact*i
+class BankAccount:
 
-     print(fact)
-  
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print("Deposit ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print("Withdraw ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print("Account balance for {} (account #{}): ₹{}".format(
+            self.__account_holder_name, self.account_number,
+            self.__account_balance))
+
+account = BankAccount(account_number="123456789",
+                      account_holder_name="Purushothaman",
+                      initial_balance=5000.0)
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
